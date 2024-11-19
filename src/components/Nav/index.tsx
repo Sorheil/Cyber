0b0db894039c0cdf336207cdf10fd8e108766159
sorheil <sorheiltchokothe@gmail.com>
2024-11-19 19:00:20 +0100

    fix: fix the navbar color

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,7 +6,7 @@ export default function Nav({ isHidden }: NavProps) {
     return (
         <nav
             className={`fixed inset-0 flex flex-col justify-center items-center text-3xl text-white gap-3 backdrop-blur-sm transition-transform duration-500 ${isHidden ? '-translate-x-full' : 'translate-x-0'
-                } md:static md:translate-x-0 md:flex-row md:gap-5 md:text-lg`}
+                } md:static md:translate-x-0 md:flex-row md:gap-5 md:text-lg md:text-black`}
         >
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
