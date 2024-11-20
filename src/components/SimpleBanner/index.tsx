999e17bbe49518b0bb1cb792bcd96c7e8e782af2
sorheil <sorheiltchokothe@gmail.com>
2024-11-20 14:45:10 +0100

    feat:-create banner       -create the second banner

--- src/components/SimpleBanner/index.tsx
+++ src/components/SimpleBanner/index.tsx
@@ -1,20 +1,55 @@
+/**
+ * -this components need to be refactorized
+ * -section apple vision pro need to be adjust
+ * 
+ */
 
-interface SimpleBannerProps {
-    id: number;
-    title: string;
-    description: string;
-    name: string
-}
-export default function SimpleBanner({ id, title, description, name }: SimpleBannerProps) {
-    const srcImg = `src/assets/products-banner/${name}.png`
-    const isDark = id % 2 === 0
+export default function SimpleBanner() {
+    const srcImg = `src/assets/products-banner/`
+
     return (
-        <article className={`flex flex-col items-center gap-3 px-4 py-10 ${isDark ? 'bg-[#353535]' : 'bg-white'}`}>
-            <div>
-                <img src={srcImg} alt={name} />
+        <section className="grid grid-cols-1 grid-rows-4 md:grid-cols-3 md:grid-rows-2">
+            {/* airpods */}
+            <div className="row-start-1 row-end-2 flex flex-col justify-center items-center py-10 px-4 bg-gray-200
+            md:row-start-2 md:row-end-3 md:col-start-1 md:col-end-2  md:flex-row md:p-0 md:pr-7">
+                <img src={srcImg + "apple-airpods-max.png"} alt="apple-airpods-max" className="md:h-full md:w-auto md:relative md:transform md:-translate-x-1/2" />
+                <div className="text-center flex flex-col gap-3 md:text-left">
+                    <h2 className="text-3xl font-light "> Apple Airpods <span className="font-semibold">Max</span> </h2>
+                    <p className="text-lg text-black/50">computational audio, Listen, it's powerful</p>
+                </div>
             </div>
-            <h3 className={`text-4xl font-light ${isDark ? 'text-white' : 'text-black'}`}>{title}</h3>
-            <p className={`text-lg text-center text-black/50 ${isDark ? 'text-white/25' : 'text-black/50'}`} >{description}</p>
-        </article>
+
+            {/* apple vision pro */}
+            <div className="row-start-2 row-end-3 flex flex-col justify-center items-center bg-black/90 py-10 px-4 
+            md:row-start-2 md:row-end-3 md:col-start-2 md:col-end-3 md:flex-row md:p-0 md:pr-7 md:overflow-hidden">
+                <img src={srcImg + "apple-vision-pro.png"} alt="apple-vison-pro" className="md:h-full md:w-auto md:relative md:transform md:-translate-x-1/3" />
+                <div className="text-center flex flex-col gap-3 ">
+                    <h2 className="text-3xl font-light text-white md:text-left"> Apple Vision  <br className="hidden md:block" /><span className="font-semibold">Pro</span></h2>
+                    <p className="text-center text-lg text-white/50 md:text-left">An immersive way to experience entertainment</p>
+                </div>
+            </div>
+
+            {/* ps5 */}
+            <div className="row-start-3 row-end-4 flex flex-col justify-center items-center py-10 px-4 
+                md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-3 md:flex-row md:p-0 md:pr-7">
+                <img src={srcImg + "playstation-5.png"} alt="playstation-5" className="md:h-full md:w-auto md:relative md:transform md:-translate-x-1/4" />
+                <div className="text-center flex flex-col gap-3">
+                    <h2 className="text-3xl font-light md:text-left"> Playstation <span className="font-semibold">5</span></h2>
+                    <p className=" md:w-1/2 text-lg text-black/50 md:text-left ">Incredibly powerful CPUs, and an SSD with integrated I/O will redefine your Playstation experience</p>
+                </div>
+            </div>
+
+            {/* macbook */}
+            <div className="row-start-4 row-end-5 flex flex-col justify-center items-center bg-gray-200 py-8 px-4 
+            md:row-start-1 md:row-end-3 md:col-start-3 md:col-end-4">
+                    <img src={srcImg + "macBook-pro-14.png"} alt="macBook-pro-14" className="md:h-full md:w-auto md:relative " />
+                    <div className=" text-center flex flex-col gap-3 ">
+                        <h2 className="text-3xl font-light "><span className="font-semibold">Macbook</span> Air</h2>
+                        <p className=" text-lg text-black/50">The new 15-inch MacBook air makes room for more of what you love with a spacious Liquid Retina display</p>
+                        <button className=' w-full text-lg border border-black/50  rounded-md py-2 px-4'>Shop Now</button>
+                    </div>
+            </div>
+
+        </section>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,37 +7,12 @@ import Banner from '../../components/Banner'
 import SimpleBanner from '../../components/SimpleBanner'
 
 export default function Home() {
-    const simpleBanner = [{
-        id: 1,
-        title: "Apple Airpods Max",
-        description: "Computational audio. Listen, it's powerful",
-        name: 'apple-airpods-max'
-    },
-    {
-        id: 2,
-        title: "Apple Vision Pro",
-        description: "An immersive way to experience entertainment",
-        name: 'apple-vision-pro'
-    },
-    {
-        id: 3,
-        title: "Playstation 5",
-        description: "Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.",
-        name: 'playstation-5'
-    }]
+
     return (<>
         <Header />
         <main>
             <Banner />
-            {simpleBanner.map((item, index) => (
-                <SimpleBanner
-                    key={index}
-                    id={item.id}
-                    title={item.title}
-                    description={item.description}
-                    name={item.name}
-                />
-            ))}
+            <SimpleBanner />
         </main>
         <Footer />
     </>
