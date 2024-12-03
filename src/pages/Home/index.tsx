1823a928c0ec29e6c11edfa828f662bae849d0d6
sorheil <sorheiltchokothe@gmail.com>
2024-12-03 20:02:34 +0100

    fix some byg on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,7 @@
 // style
 import './../../App.css'
 // components
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+
 import HeroBanner from '../../components/HeroBanner'
 import FeaturedBanner from '../../components/FeaturedBanner'
 import Categories from '../../components/Categories'
